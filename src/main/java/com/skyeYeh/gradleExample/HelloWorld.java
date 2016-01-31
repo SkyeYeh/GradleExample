645fef8c88357bc103e0ac8c5eb37fb7e2a4455b
SkyeYeh <presto408@gmail.com>
2016-01-31 23:04:37 +0800

    第一個 Gradle 專案。

---
+++ src/main/java/com/skyeYeh/gradleExample/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.skyeYeh.gradleExample;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by Skye on 2016/1/31.
+ * Hello World
+ */
+public class HelloWorld {
+    static Log logger = LogFactory.getLog(HelloWorld.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
